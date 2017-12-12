a044a1ca17d026b838fde5a1e690c7528205a5b8
lostsquirrel <songli1988@gmail.com>
2017-12-12 17:05:22 +0800

    amend to last commit

--- store/src/main/java/demo/spring/store/controller/StoreController.java
+++ store/src/main/java/demo/spring/store/controller/StoreController.java
@@ -1,4 +1,4 @@
-package demo.spring.store.demo.spring.store.controller;
+package demo.spring.store.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
