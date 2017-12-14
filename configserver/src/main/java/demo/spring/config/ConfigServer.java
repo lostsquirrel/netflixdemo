6c2dace620d31aba7d780e3591f45b9f5afe57e6
lisong <songli1988@126.com>
2017-12-14 10:08:21 +0800

    add config server

--- configserver/src/main/java/demo/spring/config/ConfigServer.java
+++ configserver/src/main/java/demo/spring/config/ConfigServer.java
@@ -2,6 +2,7 @@ package demo.spring.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
