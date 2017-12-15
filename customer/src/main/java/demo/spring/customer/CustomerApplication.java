310c871010981d26b40505b6c6bc84b70cc5cecf
lisong <songli1988@126.com>
2017-12-15 09:57:43 +0800

    add toy busines

--- customer/src/main/java/demo/spring/customer/CustomerApplication.java
+++ customer/src/main/java/demo/spring/customer/CustomerApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.hypermedia.DynamicServiceInstanceProvider;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
--- store/src/main/java/demo/spring/store/controller/StoreController.java
+++ store/src/main/java/demo/spring/store/controller/StoreController.java
@@ -1,13 +1,27 @@
 package demo.spring.store.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import demo.spring.store.dao.StoreRepository;
+import demo.spring.store.po.Store;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.Point;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 public class StoreController {
+
+    private StoreRepository storeRepository;
+
+    public StoreController(StoreRepository storeRepository) {
+        this.storeRepository = storeRepository;
+    }
+
 
     @RequestMapping("/")
     public Object home() {
@@ -15,4 +29,28 @@ public class StoreController {
         data.put("msg", "this is stores service");
         return data;
     }
+
+    @RequestMapping("/near")
+    public Page<Store> search(@RequestParam("latitude")Double latitude,
+                              @RequestParam("longitude")Double longitude,
+                              @RequestParam("distance")Double range,
+                              @RequestParam(value = "page", defaultValue = "1")Integer page,
+                              @RequestParam(value = "size", defaultValue = "10")Integer size
+                              ) {
+        Point location = new Point(longitude, latitude);
+        Distance distance = new Distance(range);
+        Pageable pageable = new PageRequest(page - 1, size);
+        return storeRepository.findByAddressLocationNear(location, distance, pageable);
+    }
+
+    @GetMapping("/stores")
+    public List<Store> stores() {
+        return storeRepository.stores();
+    }
+
+    @GetMapping("/stores/{storeId}")
+    public Store stores(@PathVariable("storeId")String storeId) {
+        return storeRepository.get(storeId);
+    }
+
 }
