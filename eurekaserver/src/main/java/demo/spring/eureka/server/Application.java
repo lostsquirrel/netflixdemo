bc3002f550c359d94e037f2db0dcd357d0e2c78f
lisong <songli1988@126.com>
2017-12-15 09:53:18 +0800

    update env file to config

--- eurekaserver/src/main/java/demo/spring/eureka/server/Application.java
+++ eurekaserver/src/main/java/demo/spring/eureka/server/Application.java
@@ -1,5 +1,7 @@
 package demo.spring.eureka.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -9,12 +11,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableAutoConfiguration
 @EnableEurekaServer
 @RestController
 public class Application {
+    private  static final Logger logger = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
+        logger.debug(Arrays.toString(args));
         new SpringApplicationBuilder(Application.class).web(true).run(args);
     }
 }
