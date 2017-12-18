0a35d4609c4c1a2fc5e6c14b69172ee439d8df4d
lostsquirrel <songli1988@gmail.com>
2017-12-18 15:03:45 +0800

    reorganize config

--- eurekaserver/src/main/java/demo/spring/eureka/server/Application.java
+++ eurekaserver/src/main/java/demo/spring/eureka/server/Application.java
@@ -2,13 +2,10 @@ package demo.spring.eureka.server;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
