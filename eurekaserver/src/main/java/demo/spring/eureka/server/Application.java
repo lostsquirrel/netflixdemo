25874d38d56c971d01dfea85606e9ee82cfa9dfa
lostsquirrel <songli1988@gmail.com>
2017-12-18 16:01:57 +0800

    change annotation

--- eurekaserver/src/main/java/demo/spring/eureka/server/Application.java
+++ eurekaserver/src/main/java/demo/spring/eureka/server/Application.java
@@ -2,18 +2,14 @@ package demo.spring.eureka.server;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableEurekaServer
-@RestController
 public class Application {
     private  static final Logger logger = LoggerFactory.getLogger(Application.class);
 
