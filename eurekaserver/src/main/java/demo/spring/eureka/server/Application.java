0f6819fb0d81ed7c52c21519bddc52915568d40f
lostsquirrel <songli1988@gmail.com>
2017-12-18 17:48:16 +0800

    add custom ip config

--- eurekaserver/src/main/java/demo/spring/eureka/server/Application.java
+++ eurekaserver/src/main/java/demo/spring/eureka/server/Application.java
@@ -2,9 +2,14 @@ package demo.spring.eureka.server;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 
 import java.util.Arrays;
 
@@ -16,5 +21,25 @@ public class Application {
     public static void main(String[] args) {
         logger.debug(Arrays.toString(args));
         new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+
+    @Value("${server.port:8761}")
+    private int port;
+
+    @Value("${eureka.instance.hostname:localhost}")
+    private String hostname;
+
+    @Value("${eureka.instance.ip-address}")
+    private String ipAddress;
+    @Bean
+    @Autowired
+    public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils inetUtils) {
+        EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
+//        AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+        config.setHostname(hostname);
+        config.setIpAddress(ipAddress);
+        config.setNonSecurePort(port);
+//        config.setDataCenterInfo(info);
+        return config;
     }
 }
