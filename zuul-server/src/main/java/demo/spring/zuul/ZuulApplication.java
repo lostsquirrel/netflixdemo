033702c1a10a77d7dec3686a93af7c588186a8b2
lostsquirrel <songli1988@gmail.com>
2017-12-26 13:27:41 +0800

    enable discovery client in zuul

--- zuul-server/src/main/java/demo/spring/zuul/ZuulApplication.java
+++ zuul-server/src/main/java/demo/spring/zuul/ZuulApplication.java
@@ -2,7 +2,9 @@ package demo.spring.zuul;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Configuration;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -10,4 +12,7 @@ public class ZuulApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(ZuulApplication.class).run(args);
     }
+    @Configuration
+    @EnableDiscoveryClient
+    static class CloudConfig {}
 }
