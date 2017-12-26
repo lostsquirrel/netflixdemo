840dd250dc6bdfc957fe377208f96e4be494513b
lostsquirrel <songli1988@gmail.com>
2017-12-26 11:40:26 +0800

    fix zuul error

--- zuul-server/src/main/java/demo/spring/zuul/ZuulApplication.java
+++ zuul-server/src/main/java/demo/spring/zuul/ZuulApplication.java
@@ -1,9 +1,11 @@
 package demo.spring.zuul;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
+@SpringBootApplication
 public class ZuulApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(ZuulApplication.class).run(args);
